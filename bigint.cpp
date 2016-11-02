653f2c72221360130574f1c8be2f79fa2270890b
ki4yvc <dawilco@koala9.cs.clemson.edu>
2016-11-01 21:28:00 -0400

    Added missing files

---
+++ bigint.cpp
@@ -0,0 +1,58 @@
+/*
+ * Name: Dylan Wilcox, Nicholas Gustafson
+ * Date: November 3rd, 2016
+ * File: bigint.cpp
+ * Description:
+ */
+
+#include "bigint.h"
+
+bigInt::bigInt()
+{
+
+}
+
+bigInt::bigInt(int n)
+{
+
+}
+
+bigInt::bigInt(bigInt &big)
+{
+
+}
+
+void bigInt::readNumber(ifstream &in)
+{
+
+}
+
+int bigInt::size()
+{
+
+}
+
+void bigInt::printReverse()
+{
+
+}
+
+void bigInt::print()
+{
+
+}
+
+bigInt* bigInt::add(bigInt &big2)
+{
+
+}
+
+bigInt* bigInt::operator+(bigInt &big2)
+{
+
+}
+
+bigInt* bigInt::multiply(bigInt &big2)
+{
+
+}
